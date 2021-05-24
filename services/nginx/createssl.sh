47f89f0779be7155086b076e6455f198a0c1fda8
Christian Mazariegos <christianmaza97@gmail.com>
2021-05-24 14:33:12 -0400

    Fourth commit - implemented nginx as a reverse proxy and Dockerized the application

---
+++ services/nginx/createssl.sh
@@ -0,0 +1,18 @@
+cd /usr/local/src/
+wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz 
+tar -xf openssl-1.1.1g.tar.gz
+cd openssl-1.1.1g
+./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+make
+make install
+cd
+mkdir local_ssl
+cd local_ssl
+touch ssl.conf
+echo -e '[req]\ndistinguished_name = req_distinguished_name\nx509_extensions = v3_req\nprompt = no\n[req_distinguished_name]\nC = US\nST = FL\nL = OCALA\nO = HP-AUTOMOTIVE\nOU = AUTO_MECHANIC\nCN = HP_AUTO\n[v3_req]\nkeyUsage = keyEncipherment, dataEncipherment\nextendedKeyUsage = serverAuth\nsubjectAltName = @alt_names\n[alt_names]\nDNS.1 = hpautoia.com\n' > ssl.conf
+openssl req -x509 -nodes -days 1024 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config ssl.conf -extensions 'v3_req'
+cp localhost.crt /etc/ssl/certs/localhost.crt
+cp localhost.key /etc/ssl/private/localhost.key
+mkdir -p $HOME/.pki/nssdb
+certutil -d $HOME/.pki/nssdb -N
+certutil -d sql:$HOME/.pki/nssdb -A -t "CT,c,c" -n "localhost" -i localhost.crt
