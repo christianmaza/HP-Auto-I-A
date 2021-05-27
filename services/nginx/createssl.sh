dee890f80f6efef098fe1a35dedd44eb1123d7de
Christian Mazariegos <43866744+christianmaza@users.noreply.github.com>
2021-05-27 09:25:43 -0400

    Deleted certificate information from createssl.sh

--- services/nginx/createssl.sh
+++ services/nginx/createssl.sh
@@ -9,7 +9,7 @@ cd
 mkdir local_ssl
 cd local_ssl
 touch ssl.conf
-echo -e '[req]\ndistinguished_name = req_distinguished_name\nx509_extensions = v3_req\nprompt = no\n[req_distinguished_name]\nC = US\nST = FL\nL = OCALA\nO = HP-AUTOMOTIVE\nOU = AUTO_MECHANIC\nCN = HP_AUTO\n[v3_req]\nkeyUsage = keyEncipherment, dataEncipherment\nextendedKeyUsage = serverAuth\nsubjectAltName = @alt_names\n[alt_names]\nDNS.1 = hpautoia.com\n' > ssl.conf
+echo -e '[req]\ndistinguished_name = req_distinguished_name\nx509_extensions = v3_req\nprompt = no\n[req_distinguished_name]\nC = **Country**\nST = **State**\nL = **City**\nO = **Organization name**\nOU = **Department**\nCN = **Certificate Issuer**\n[v3_req]\nkeyUsage = keyEncipherment, dataEncipherment\nextendedKeyUsage = serverAuth\nsubjectAltName = @alt_names\n[alt_names]\nDNS.1 = **Domain name**\n' > ssl.conf
 openssl req -x509 -nodes -days 1024 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config ssl.conf -extensions 'v3_req'
 cp localhost.crt /etc/ssl/certs/localhost.crt
 cp localhost.key /etc/ssl/private/localhost.key
